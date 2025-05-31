cdf35392a0b26fa432a9680ac19fe0bf547daa85
ehtnamuh <saminbinkarim@gmail.com>
2025-05-31 18:39:14 -0500

    Static rendering for github

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {output:"standalone"}
+// const nextConfig = {output:"standalone"}
+const nextConfig = {
+  output: "export",
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // <-- Add this line
+  },
+};
+
 
 module.exports = nextConfig
